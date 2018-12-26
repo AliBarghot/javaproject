286312c7d6dfb7af049c4f13f537a6d418b5894e
= <=>
2018-12-26 14:37:47 +0200

    second commit

--- src/javaproject/Javaproject.java
+++ src/javaproject/Javaproject.java
@@ -16,7 +16,7 @@ public class Javaproject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hellow");
+        System.out.println("hellow2");
     }
     
 }
